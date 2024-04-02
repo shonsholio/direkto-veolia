5722f434e78db246f07c933ee05e4f417deab8e0
Luis Fernando Arevalo <LuisFerArevalo@Luiss-MacBook-Pro.local>
2024-04-02 14:26:19 -0500

    algunas fotos

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -6,5 +6,9 @@ controller.main = (req,res) => {
   res.render('main')
 }
 
+controller.info = (req,res) => {
+  res.render('plantilla')
+}
+
 
 module.exports = controller
--- routes/main.js
+++ routes/main.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const mainController = require('../controllers/mainController')
 
 router.get('/', mainController.main)
+router.get('/op1', mainController.info)
+router.get('/op2', mainController.info)
+
 
 
 module.exports = router 
